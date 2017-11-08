fe5c77acfc5b36c31cc86590290d32b574e93bbb
WojciechKasprzyk <w.kasprzyk95@gmail.com>
2017-11-08 21:43:54 +0100

    after audio

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+	public GameObject explosion;
+	public GameObject playerExplosion;
+	void OnTriggerEnter(Collider other) {
+		if("Boundary" == other.tag) {
+			return;
+		}
+		Instantiate(explosion, transform.position, transform.rotation);
+		if ("Player" == other.tag){
+			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+		}
+		Destroy(other.gameObject);
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boudary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public float speed;
+    public float tilt;
+    public Boudary boundary;
+    public GameObject shot;
+    public Transform shotSpawn;
+    private float nextFire = 0.0f;
+    public float fireRate;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            GetComponent<AudioSource>().Play();
+        }
+    }
+    void FixedUpdate()
+    {
+        float moveHorisontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorisontal, 0.0f, moveVertical);
+
+
+        rb.velocity = movement * speed;
+
+        rb.position = new Vector3
+        (
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+        );
+
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.position.x * -tilt);
+
+    }
+
+}
+
